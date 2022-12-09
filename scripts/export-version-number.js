ea357c9080700ab20b311885b209b23c4a39cdce
Avior <f.bouillon@aptatio.com>
2022-12-09 16:08:10 +0100

    fix: Version number not being correctly exported
    Signed-off-by: Avior <f.bouillon@aptatio.com>

--- scripts/export-version-number.js
+++ scripts/export-version-number.js
@@ -1,4 +1,4 @@
 const { version } = require('../package.json')
 const fs = require('fs')
 
-fs.writeFileSync('./src/version.js', `export const version = ${JSON.stringify({version})}`)
+fs.writeFileSync('./src/version.js', `export const version = '${version}'`)
